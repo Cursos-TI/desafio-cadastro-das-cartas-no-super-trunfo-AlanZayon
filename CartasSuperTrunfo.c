5b635307ecdf7b55d20544b50f90bda260a71c3c
Alan Zayon <alanzayon72@gmail.com>
2025-02-10 13:21:08 +0000

    desafio aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,6 +16,7 @@ int main()
     float area;
     float pib;
     int pontosTuristicos;
+    float densidadePopulacional, pibPerCapita;
 
     // Entrada dos dados da cidade
     printf("Estado (A-H): ");
@@ -33,14 +34,20 @@ int main()
     printf("Numero de pontos turisticos: ");
     scanf("%d", &pontosTuristicos);
 
+    // Cálculo das novas propriedades
+    densidadePopulacional = (area > 0) ? populacao / area : 0;
+    pibPerCapita = (populacao > 0) ? (pib * 1000000) / populacao : 0;
+
     // Exibição dos dados da cidade
-    printf("\nDados da cidade :\n");
+    printf("\nDados da cidade:\n");
     printf("Codigo: %c%02d\n", estado, numeroCidade);
     printf("Nome: %s\n", nome);
     printf("Populacao: %d\n", populacao);
     printf("Area: %.2f km²\n", area);
     printf("PIB: %.2f milhoes\n", pib);
     printf("Pontos Turisticos: %d\n", pontosTuristicos);
+    printf("Densidade Populacional: %.2f habitantes/km²\n", densidadePopulacional);
+    printf("PIB per Capita: %.2f milhoes\n", pibPerCapita);
 
     return 0;
 }
